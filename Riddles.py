84d452be6fb5dd4206994df680c756412a883c00
Gurleen Kaur <73643543+gurleenMaini@users.noreply.github.com>
2021-12-29 13:32:00 +0530

    Add files via upload

---
+++ Riddles.py
@@ -0,0 +1,30 @@
+print("Let's have some fun time with riddles!")
+riddle = input("What has many keys but can't open a single lock? ")
+    if(riddle=="A piano"):
+        print("You got this one right!")
+    else:
+        print("Oops! That's not the right answer")
+
+riddle= input("What five-letter word becomes shorter when you add two letters to it?")
+    if(riddle=="Shorter"):
+        print("You got this one right!")
+    else:
+        print("Oops! That's not the right answer")    
+
+riddle= input("What begins with an “e” and only contains one letter?")
+    if(riddle== "An envelope"):
+        print("You got this one right!")
+    else:
+        print("Oops! That's not the right answer")   
+
+riddle= input("Which bank doesn't contains money?"):
+    if(riddle== "A river bank"):
+        print("You got this one right!")
+    else:
+        print("Oops! That's not the right answer")  
+
+riddle= input("What can you break, even if you never pick it up or touch it?")
+    if(riddle== "A promise"):
+         print("You got this one right!")
+    else:
+        print("Oops! That's not the right answer") 
